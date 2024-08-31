3bf1cd9a54c8f02eb41005ef2afaf801c54f5414
Mariano <Marianonicolaslosada@gmail.com>
2024-08-31 18:10:07 -0300

    Entrega tp 2

--- src/componentes/NavigationComponent/navbar.jsx
+++ src/componentes/NavigationComponent/navbar.jsx
@@ -1,7 +1,7 @@
 import './navbar.css';
 import { Link } from 'react-router-dom';
 
-// Los botones crean el formato de navegación con enlaces a las categorías
+
 export default function NavBar() {
   return (
     <nav
